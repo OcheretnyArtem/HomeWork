d425191223a0fba0b147accfea0a18a61c59e5f6
Ocheretny Artem <ocheretnyartem@gmail.ru>
2021-04-14 01:49:24 +0300

    first task - Done

---
+++ app/src/main/java/by/ocheretny/homework/homework2/HW2ActivityTask2.kt
@@ -0,0 +1,13 @@
+package by.ocheretny.homework.homework2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.ocheretny.homework.R
+
+class HW2ActivityTask2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw2_task2)
+
+    }
+}
