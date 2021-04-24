0be498031a285dd06b6409fa1509c57a8aaba1e2
Ocheretny Artem <ocheretnyartem@gmail.ru>
2021-04-24 14:43:27 +0300

    Reworked display on the show-fragment

--- app/src/main/java/by/ocheretny/homework/homework4/HW4FragmentShow.kt
+++ app/src/main/java/by/ocheretny/homework/homework4/HW4FragmentShow.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import by.ocheretny.homework.R
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_hw4_show.*
 
 class HW4FragmentShow : Fragment() {
@@ -28,37 +26,19 @@ class HW4FragmentShow : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val adapter = SnowdropsAdapter(requireContext())
+
         viewModel = ViewModelProvider(requireActivity()).get(HW4ViewModel::class.java)
 
-        val snowdropsNames = getNameOfSnowdrop(viewModel.snowdrops)
+        snowdrop_recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        snowdrop_recyclerView.adapter = adapter
 
-        val arrayAdapter = ArrayAdapter(
-            requireContext(),
-            R.layout.support_simple_spinner_dropdown_item,
-            snowdropsNames
-        )
+        viewModel.snowdrops.observe(requireActivity()){
 
-        spinner_snowdrop.adapter = arrayAdapter
-
-        spinner_snowdrop.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                viewModel.snowdrops.observe(requireActivity()) {
-                    days_description_snowdrop.text = it[position].description
-                    Picasso.get().load(it[position].ImageURL).into(image_of_snowdrop)
-                }
+            if (!it.isNullOrEmpty()){
+                adapter.setItems(it)
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
         }
     }
-}
-
-private fun getNameOfSnowdrop(list: LiveData<ArrayList<Snowdrop>>): Array<String> {
-    return list.value?.map { it.name }?.toTypedArray() ?: arrayOf("")
 }
