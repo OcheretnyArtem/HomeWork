6847d81502d220e97de5f5c6939ff29e181ef3bf
Ocheretny Artem <ocheretnyartem@gmail.ru>
2021-05-17 15:36:54 +0300

    improved display

--- app/src/main/java/by/ocheretny/homework/homework9/recycler/CoinAdapter.kt
+++ app/src/main/java/by/ocheretny/homework/homework9/recycler/CoinAdapter.kt
@@ -3,6 +3,7 @@ package by.ocheretny.homework.homework9.recycler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import by.ocheretny.homework.R
@@ -15,10 +16,25 @@ class CoinAdapter(val coins: List<Crypto>) : RecyclerView.Adapter<CoinAdapter.Cr
         fun setData(itemView: View, position: Int) {
 
             val crypto = coins[position]
+            val image = itemView.findViewById<ImageView>(R.id.hw9_trending)
+
+            when {
+                crypto.percentChange1h ?: 0.0 > 0.0 -> {
+                    image.setImageResource(R.drawable.trending_up)
+                }
+                crypto.percentChange1h ?: 0.0 < 0.0 -> {
+                    image.setImageResource(R.drawable.trending_down)
+                }
+                crypto.percentChange1h ?: 0.0 == 0.0 -> {
+                    image.setImageResource(R.drawable.trending_flat)
+                }
+            }
 
             itemView.findViewById<TextView>(R.id.hw9_name).text = crypto.name
-            itemView.findViewById<TextView>(R.id.hw9_percent).text = crypto.percentChange1h.toString()
-            itemView.findViewById<TextView>(R.id.hw9_price).text = crypto.price.toString()
+            itemView.findViewById<TextView>(R.id.hw9_percent).text =
+                "${crypto.percentChange1h.toString()}${view.context.getString(R.string.percent)}"
+            itemView.findViewById<TextView>(R.id.hw9_price).text =
+                "${String.format("%.2f", crypto.price)} ${view.context.getString(R.string.dollar)}"
             itemView.findViewById<TextView>(R.id.hw9_symbol).text = crypto.symbol
 
         }
