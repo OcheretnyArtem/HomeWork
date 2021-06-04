b11931b2c1d0362d0f868ab3c37df3057114c303
Ocheretny Artem <ocheretnyartem@gmail.ru>
2021-06-04 17:23:23 +0300

    add HW12Activity.kt

---
+++ app/src/main/java/by/ocheretny/homework/homework12/HW12Activity.kt
@@ -0,0 +1,12 @@
+package by.ocheretny.homework.homework12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import by.ocheretny.homework.R
+
+class HW12Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hw12)
+    }
+}
